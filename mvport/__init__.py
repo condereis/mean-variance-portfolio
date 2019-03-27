b718eea3a0b5c708d9fb8781e698bd1a39c88fbb
condereis <rafael.lcreis@gmail.com>
2019-03-27 07:00:21 -0300

    Bump version: 1.1.1 → 1.2.0

--- mvport/__init__.py
+++ mvport/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Rafael Lopes Conde dos Reis"""
 __email__ = 'rafael.lcreis@gmail.com'
-__version__ = '1.1.1'
+__version__ = '1.2.0'
 
 from .stock import Stock
 from .portfolio import Portfolio
